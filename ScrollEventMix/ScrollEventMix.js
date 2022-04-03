61cf07ca62ad682725b63a0c7ee60bbd8629cbda
wzk <2434485443@qq.com>
2022-04-03 11:31:30 +0800

    feat: 整理ScrollEventMix

--- ScrollEventMix/ScrollEventMix.js
+++ ScrollEventMix/ScrollEventMix.js
@@ -4,46 +4,48 @@
  * @class ScrollEventMix
  * @param [Element] scrollParent 有滚动条的父级元素
  */
- export default class ScrollEventMix {
+export default class ScrollEventMix {
   constructor(scrollParent) {
+    // 任务队列
+    this._TaskList = []
+    this.scrollParent = scrollParent
+
     // 父级元素的必须设置 position css 属性
-    let position = getComputedStyle(scrollParent).position;
+    let position = getComputedStyle(scrollParent).position
     if (!['absolute', 'relative', 'fixed'].includes(position)) {
-      scrollParent.style.position = 'relative';
-      console.log('添加 position:relative');
+      scrollParent.style.position = 'relative'
     }
 
-    // 事件处理函数注册表
-    this._handlers = [];
-    this.scrollParent = scrollParent;
-    this._updateScrollTop();
+    this._setViewport()
 
     // 监听 scrollParent 滚动事件
     scrollParent.addEventListener('scroll', (e) => {
       requestAnimationFrame(() => {
-        this._onScroll(e);
-      });
-    });
+        this._onScroll(e)
+      })
+    })
   }
 
   /**
-   * 注册处理函数
+   * 添加处理函数
    * @method
    * @param {string} type 处理类型 可选项：view、scroll
    * @param {function} handler 用于事件触发的回调函数
-   * @param {boolean} immediately 立即触发
+   * @param {boolean} immediately 立即执行
    * @memberof ScrollEventMix
    */
-  addHandler(type, handler, immediately = false) {
-    const typeHandler = {
+  addEventListener(type, handler, immediately = false) {
+    // 任务
+    const task = {
       type,
       handler,
-    };
+    }
 
     // 注册处理函数
-    this._handlers.push(typeHandler);
+    this._TaskList.push(task)
 
-    if (immediately) this._triggerHandler(typeHandler);
+    // 立即触发一次
+    if (immediately) this._triggerHandler(task)
   }
 
   /**
@@ -68,57 +70,56 @@
         top: 0,
         bottom: 0,
       },
-    } = config;
+    } = config
 
-    let { scrollTop, scrollBottom } = this;
-    let { offsetTop: elTop, offsetHeight: elHeight } = currentEl;
-    let elBottom = elTop + elHeight;
-
-    
+    let { scrollTop, scrollBottom } = this
+    let { offsetTop: elTop, offsetHeight: elHeight } = currentEl
+    let elBottom = elTop + elHeight
 
     // 设置可视区域
     if (range && range.top && range.bottom) {
-      scrollTop += range.top || 0;
-      scrollBottom -= range.bottom || 0;
+      scrollTop += range.top || 0
+      scrollBottom -= range.bottom || 0
     }
 
-    return elTop + delay < scrollBottom && elBottom - delay > scrollTop;
+    return elTop + delay < scrollBottom && elBottom - delay > scrollTop
   }
 
   // 每次滚动时触发
   _onScroll(e) {
-    this._updateScrollTop();
-    this._triggerHandlerAll(e);
+    this._setViewport()
+    this._triggerHandlerAll(e)
   }
 
-  // 更新 scrollTop 属性
-  _updateScrollTop() {
-    let { scrollTop, clientHeight } = this.scrollParent;
-    this.scrollBottom = scrollTop + clientHeight;
+  // 设置视口上下边界,
+  // 即当前视口显示区域的顶部和底部位置, 子元素的位置在其中间则判断为出现在了视口中
+  _setViewport() {
+    let { scrollTop, clientHeight } = this.scrollParent
 
-    this.scrollTop = scrollTop;
+    this.scrollBottom = scrollTop + clientHeight
+    this.scrollTop = scrollTop
   }
 
-  // 触发所有 handler
+  // 触发所有任务
   _triggerHandlerAll() {
-    this._handlers.forEach((handler) => {
-      this._triggerHandler(handler);
-    });
+    this._TaskList.forEach((handler) => {
+      this._triggerHandler(handler)
+    })
   }
 
-  // 触发指定 handler
+  // 触发指定任务
   _triggerHandler({ type, handler }, e = null) {
     switch (type) {
       case 'view':
-        handler(this.isView.bind(this));
-        break;
+        handler(this.isView.bind(this))
+        break
 
       case 'scroll':
-        handler(e);
-        break;
+        handler(e)
+        break
 
       default:
-        break;
+        break
     }
   }
 }
--- ScrollEventMix/index.js
+++ ScrollEventMix/index.js
@@ -1,15 +1,22 @@
-import ScrollEventMix from './ScrollEventMix.js';
+import ScrollEventMix from './ScrollEventMix.js'
 
-let scrollParent = document.querySelector('#scrollParent');
-let subDiv = document.querySelectorAll('.subDiv');
+let scrollParent = document.querySelector('#scrollParent')
+let subDiv = document.querySelectorAll('.subDiv')
 
 function subDivShow(isView) {
   subDiv.forEach((item) => {
-    if (isView(item)) {
-      item.classList.remove('hidden');
+    if (
+      isView(item, {
+        range: {
+          top: 100,
+          bottom: 100,
+        },
+      })
+    ) {
+      item.classList.remove('hidden')
     }
-  });
+  })
 }
 
-const sh = new ScrollEventMix(scrollParent);
-sh.addHandler('view', subDivShow, true);
+const sh = new ScrollEventMix(scrollParent)
+sh.addEventListener('view', subDivShow, true)
